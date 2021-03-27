fc502570e7cbeca94f1ad2b28b2ff00353357e33
yu3906 <yu3906@qq.com>
2021-03-27 22:17:22 +0800

    none

---
+++ lib/src/ui/sex_pick.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../bean/sex.dart';
+
+class SexPickPage extends StatelessWidget {
+  final String title;
+  final Sex? sex;
+
+  SexPickPage({
+    Key? key,
+    this.title = "选择性别",
+    this.sex,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            child: Material(
+              type: MaterialType.transparency,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "男",
+                          style: TextStyle(
+                              fontSize: 44.sp,
+                              color:
+                                  Sex.male == sex ? Theme.of(context).primaryColor : Colors.black),
+                        ),
+                      ),
+                      if (Sex.male == sex)
+                        Icon(Icons.radio_button_checked, color: Theme.of(context).primaryColor),
+                    ],
+                  ),
+                ),
+                onTap: () {
+                  Navigator.pop(context, Sex.male == sex ? null : Sex.male);
+                },
+              ),
+            ),
+          ),
+          SizedBox(height: 1),
+          Container(
+            color: Colors.white,
+            child: Material(
+              type: MaterialType.transparency,
+              child: InkWell(
+                child: Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          "女",
+                          style: TextStyle(
+                              fontSize: 44.sp,
+                              color: Sex.female == sex
+                                  ? Theme.of(context).primaryColor
+                                  : Colors.black),
+                        ),
+                      ),
+                      if (Sex.female == sex)
+                        Icon(Icons.radio_button_checked, color: Theme.of(context).primaryColor),
+                    ],
+                  ),
+                ),
+                onTap: () {
+                  Navigator.pop(context, Sex.female == sex ? null : Sex.female);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
