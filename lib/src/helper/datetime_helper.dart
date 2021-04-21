632f9ecb54e351853753f37ed6d4afabdba42fa4
yu3906 <yu3906@qq.com>
2021-04-21 21:40:28 +0800

    none

--- lib/src/helper/datetime_helper.dart
+++ lib/src/helper/datetime_helper.dart
@@ -1,46 +1,47 @@
 import 'package:intl/intl.dart';
 
+final DateFormat _HHmm = DateFormat("HH:mm");
+final DateFormat _MMddHHmm = DateFormat("MM-dd HH:mm");
+final DateFormat _yyyyMMdd = DateFormat("yyyy-MM-dd");
+
 class DateTimeHelper {
   static String? format(DateTime? dateTime, String? pattern) {
     if (dateTime == null || pattern == null) return null;
     return DateFormat(pattern).format(dateTime);
   }
 
-  static String? fromNow(DateTime? dateTime, [bool withoutPrefixOrSuffix = false]) {
+  static String? smartFormat(DateTime? dateTime) {
     if (dateTime == null) return null;
-    Duration diff = DateTime.now().difference(dateTime);
-    String timeString;
-    if (diff.inSeconds.abs() < 45) {
-      timeString = "几秒";
-    } else if (diff.inMinutes.abs() < 2) {
-      timeString = "1 分钟";
-    } else if (diff.inMinutes.abs() < 45) {
-      timeString = "${diff.inMinutes.abs()} 分钟";
-    } else if (diff.inHours.abs() < 2) {
-      timeString = "1 小时";
-    } else if (diff.inHours.abs() < 22) {
-      timeString = "${diff.inHours.abs()} 小时";
-    } else if (diff.inDays.abs() < 2) {
-      timeString = "1 天";
-    } else if (diff.inDays.abs() < 26) {
-      timeString = "${diff.inDays.abs()} 天";
-    } else if (diff.inDays.abs() < 60) {
-      timeString = "1 个月";
-    } else if (diff.inDays.abs() < 320) {
-      timeString = "${diff.inDays.abs() ~/ 30} 个月";
-    } else if (diff.inDays.abs() < 547) {
-      timeString = "1 年";
+    DateTime now = DateTime.now();
+    Duration diff = now.difference(dateTime);
+    if (diff.isNegative) {
+      if (diff.inSeconds.abs() < 60) {
+        return "几秒后";
+      } else if (diff.inMinutes.abs() < 2) {
+        return "1分钟后";
+      } else if (diff.inMinutes.abs() < 60) {
+        return "${diff.inMinutes.abs()} 分钟后";
+      } else if (diff.inDays.abs() == 1) {
+        return "明天 ${_HHmm.format(dateTime)}";
+      }
     } else {
-      timeString = "${diff.inDays.abs() ~/ 356} 年";
-    }
-    if (!withoutPrefixOrSuffix) {
-      if (diff.isNegative) {
-        timeString = "$timeString后";
-      } else {
-        timeString = "$timeString前";
+      if (diff.inSeconds.abs() < 60) {
+        return "刚刚";
+      } else if (diff.inMinutes.abs() < 2) {
+        return "1分钟前";
+      } else if (diff.inMinutes.abs() < 60) {
+        return "${diff.inMinutes.abs()} 分钟前";
+      } else if (diff.inDays.abs() == 1) {
+        return "昨天 ${_HHmm.format(dateTime)}";
       }
     }
-    return timeString;
+    if (now.year == dateTime.year && now.month == dateTime.month && now.day == dateTime.day) {
+      return _HHmm.format(dateTime);
+    } else if (now.year == dateTime.year && now.month == dateTime.month) {
+      return _MMddHHmm.format(dateTime);
+    } else {
+      return _yyyyMMdd.format(dateTime);
+    }
   }
 
   DateTimeHelper._();
