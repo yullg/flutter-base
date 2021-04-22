731f54f83e3fc31761e605de28f4a3a199f23ff1
yu3906 <yu3906@qq.com>
2021-04-22 20:02:59 +0800

    none

--- lib/src/helper/datetime_helper.dart
+++ lib/src/helper/datetime_helper.dart
@@ -21,8 +21,6 @@ class DateTimeHelper {
         return "1分钟后";
       } else if (diff.inMinutes.abs() < 60) {
         return "${diff.inMinutes.abs()} 分钟后";
-      } else if (diff.inDays.abs() == 1) {
-        return "明天 ${_HHmm.format(dateTime)}";
       }
     } else {
       if (diff.inSeconds.abs() < 60) {
@@ -31,17 +29,46 @@ class DateTimeHelper {
         return "1分钟前";
       } else if (diff.inMinutes.abs() < 60) {
         return "${diff.inMinutes.abs()} 分钟前";
-      } else if (diff.inDays.abs() == 1) {
-        return "昨天 ${_HHmm.format(dateTime)}";
       }
     }
-    if (now.year == dateTime.year && now.month == dateTime.month && now.day == dateTime.day) {
-      return _HHmm.format(dateTime);
-    } else if (now.year == dateTime.year && now.month == dateTime.month) {
-      return _MMddHHmm.format(dateTime);
-    } else {
-      return _yyyyMMdd.format(dateTime);
+    if (isYesterday(dateTime, now)) {
+      return "昨天 ${_HHmm.format(dateTime)}";
     }
+    if (isTomorrow(dateTime, now)) {
+      return "明天 ${_HHmm.format(dateTime)}";
+    }
+    if (isSameDay(now, dateTime)) {
+      return _HHmm.format(dateTime);
+    }
+    if (isSameYear(now, dateTime)) {
+      return _MMddHHmm.format(dateTime);
+    }
+    return _yyyyMMdd.format(dateTime);
+  }
+
+  static bool isSameYear(DateTime? dt1, DateTime? dt2) {
+    if (dt1 == null || dt2 == null) return false;
+    return dt1.year == dt2.year;
+  }
+
+  static bool isSameMonth(DateTime? dt1, DateTime? dt2) {
+    if (dt1 == null || dt2 == null) return false;
+    return dt1.year == dt2.year && dt1.month == dt2.month;
+  }
+
+  static bool isSameDay(DateTime? dt1, DateTime? dt2) {
+    if (dt1 == null || dt2 == null) return false;
+    return dt1.year == dt2.year && dt1.month == dt2.month && dt1.day == dt2.day;
+  }
+
+  static bool isYesterday(DateTime? dateTime, [DateTime? now]) {
+    if (dateTime == null) return false;
+    return isSameDay(dateTime.add(Duration(days: 1)), now ?? DateTime.now());
+  }
+
+  static bool isTomorrow(DateTime? dateTime, [DateTime? now]) {
+    if (dateTime == null) return false;
+    return isSameDay(dateTime.add(Duration(days: -1)), now ?? DateTime.now());
   }
 
   DateTimeHelper._();
