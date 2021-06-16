d7cbb191486bc63f277a50a8201657d69a2d204c
yu5932 <yu5932@qq.com>
2021-06-16 11:40:11 +0800

    none

--- lib/src/logger/system_logger.dart
+++ lib/src/logger/system_logger.dart
@@ -1,5 +1,19 @@
 class SystemLogger {
-  static void log(dynamic message, [dynamic error, StackTrace? trace]) {}
+  static void log(dynamic message, [dynamic error, StackTrace? trace]) {
+    try {
+      print(_stringifyLog(message, error, trace));
+    } catch (e) {}
+  }
+
+  static String _stringifyLog(dynamic message, [dynamic error, StackTrace? trace]) {
+    StringBuffer result = StringBuffer();
+    result.write("${message ?? '---'}    ");
+    result.writeln("${error ?? '---'}");
+    if (trace != null) {
+      result.writeln(trace);
+    }
+    return result.toString();
+  }
 
   SystemLogger._();
 }
