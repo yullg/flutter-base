e71893319fdd583fe6107f929769e2d3e8797b74
yu5932 <yu5932@qq.com>
2021-07-09 17:56:46 +0800

    none

--- lib/src/ui/widget/badge_widget.dart
+++ lib/src/ui/widget/badge_widget.dart
@@ -1,57 +1,75 @@
 import 'package:flutter/material.dart';
 
+enum _Mode { dot, num }
+
 class BadgeWidget extends StatelessWidget {
-  final int count;
-  final int overflowCount;
-  final double dotDiameter;
+  final _Mode _mode;
   final Color color;
   final Color backgroundColor;
-  final double fontSize;
   final Color borderColor;
   final double borderWidth;
+  final double dotDiameter;
+  final int count;
+  final int maxCount;
+  final double fontSize;
 
-  const BadgeWidget(
+  const BadgeWidget.dot(
       {Key? key,
-      required this.count,
-      this.overflowCount = 99,
-      this.dotDiameter = 10,
       this.color = Colors.white,
       this.backgroundColor = Colors.red,
-      this.fontSize = 12,
       this.borderColor = Colors.white,
-      this.borderWidth = 1})
-      : super(key: key);
+      this.borderWidth = 1,
+      this.dotDiameter = 10})
+      : this._mode = _Mode.dot,
+        this.count = 0,
+        this.maxCount = 0,
+        this.fontSize = 0,
+        super(key: key);
+
+  const BadgeWidget.num({
+    Key? key,
+    this.color = Colors.white,
+    this.backgroundColor = Colors.red,
+    this.borderColor = Colors.white,
+    this.borderWidth = 1,
+    required this.count,
+    this.maxCount = 99,
+    this.fontSize = 12,
+  })  : this._mode = _Mode.num,
+        this.dotDiameter = 0,
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (count <= 0) {
-      return Container(
-        width: dotDiameter,
-        height: dotDiameter,
-        decoration: BoxDecoration(
-          color: backgroundColor,
-          shape: BoxShape.circle,
-          border: Border.all(color: borderColor, width: borderWidth),
-        ),
-      );
-    } else {
-      var data = count <= overflowCount ? "$count" : "$overflowCount+";
-      if (data.length <= 2) {
+    switch (_mode) {
+      case _Mode.dot:
         return Container(
-          padding: EdgeInsets.all(1),
-          decoration: BoxDecoration(color: backgroundColor, shape: BoxShape.circle, border: Border.all(color: borderColor, width: borderWidth)),
-          child: Text(data, style: TextStyle(color: color, fontSize: fontSize)),
-        );
-      } else {
-        return Container(
-          padding: EdgeInsets.fromLTRB(2, 0, 2, 0),
-          decoration: ShapeDecoration(
+          width: dotDiameter,
+          height: dotDiameter,
+          decoration: BoxDecoration(
             color: backgroundColor,
-            shape: StadiumBorder(side: BorderSide(color: borderColor, width: borderWidth)),
+            shape: BoxShape.circle,
+            border: Border.all(color: borderColor, width: borderWidth),
           ),
-          child: Text(data, style: TextStyle(color: color, fontSize: fontSize)),
         );
-      }
+      case _Mode.num:
+        var data = count <= maxCount ? "$count" : "$maxCount+";
+        if (data.length <= 2) {
+          return Container(
+            padding: EdgeInsets.all(4 / data.length),
+            decoration: BoxDecoration(color: backgroundColor, shape: BoxShape.circle, border: Border.all(color: borderColor, width: borderWidth)),
+            child: Text(data, style: TextStyle(color: color, fontSize: fontSize)),
+          );
+        } else {
+          return Container(
+            padding: EdgeInsets.fromLTRB(2, 0, 2, 0),
+            decoration: ShapeDecoration(
+              color: backgroundColor,
+              shape: StadiumBorder(side: BorderSide(color: borderColor, width: borderWidth)),
+            ),
+            child: Text(data, style: TextStyle(color: color, fontSize: fontSize)),
+          );
+        }
     }
   }
 }
