d7876143ba423591ddbd532040bf27154e2dd9db
Admin <admin@AdmindeMac-mini.local>
2021-07-23 23:10:43 +0800

    none

--- lib/src/ui/widget/predicate_widget.dart
+++ lib/src/ui/widget/predicate_widget.dart
@@ -1,72 +1,54 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:provider/provider.dart';
 
 import '../../permission/permission.dart';
+import 'rebuild_widget.dart';
 
 class PredicateWidget extends StatefulWidget {
   final Predicate predicate;
-  final ValueWidgetBuilder<PredicateStatus> childBuilder;
+  final WidgetBuilder trueWidgetBuilder;
+  final WidgetBuilder falseWidgetBuilder;
+  final WidgetBuilder waitingWidgetBuilder;
+  final ValueWidgetBuilder<Object> failedWidgetBuilder;
 
-  const PredicateWidget({Key? key, required this.predicate, required this.childBuilder}) : super(key: key);
+  const PredicateWidget({
+    Key? key,
+    required this.predicate,
+    required this.trueWidgetBuilder,
+    this.falseWidgetBuilder = _defaultFalseWidgetBuilder,
+    this.waitingWidgetBuilder = _defaultWaitingWidgetBuilder,
+    this.failedWidgetBuilder = _defaultFailedWidgetBuilder,
+  }) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _PredicateState();
-
-  static void refresh(BuildContext context) {
-    context.findAncestorStateOfType<_PredicateState>()?.controller.refresh();
-  }
 }
 
 class _PredicateState extends State<PredicateWidget> {
-  late final _PredicateController controller;
+  late final Future<bool> future;
 
   @override
   void initState() {
     super.initState();
-    controller = _PredicateController(context, widget.predicate);
-    controller.refresh();
+    future = widget.predicate.test(context);
   }
 
-  Widget build(BuildContext context) => ChangeNotifierProvider.value(
-        value: controller,
-        child: Consumer<_PredicateController>(
-          builder: (context, notifier, child) => widget.childBuilder(context, notifier.status, child),
-        ),
+  Widget build(BuildContext context) => FutureBuilder<bool>(
+        future: future,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return widget.waitingWidgetBuilder(context);
+          } else if (snapshot.hasError) {
+            return widget.failedWidgetBuilder(context, snapshot.error!, null);
+          } else {
+            return snapshot.requireData ? widget.trueWidgetBuilder(context) : widget.falseWidgetBuilder(context);
+          }
+        },
       );
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
 }
-
-class _PredicateController extends ChangeNotifier {
-  final BuildContext context;
-  final Predicate predicate;
-  PredicateStatus _status;
-
-  _PredicateController(this.context, this.predicate) : _status = PredicateStatus.initial;
-
-  PredicateStatus get status => _status;
-
-  set status(PredicateStatus value) {
-    _status = value;
-    notifyListeners();
-  }
-
-  Future<void> refresh() async {
-    status = PredicateStatus.testing;
-    await predicate
-        .test(context)
-        .then((value) => value ? status = PredicateStatus.test_success : status = PredicateStatus.test_fail)
-        .catchError((e) => status = PredicateStatus.test_fail);
-  }
-}
-
-enum PredicateStatus { initial, testing, test_fail, test_success }
 
 abstract class Predicate {
   Future<bool> test(BuildContext context);
@@ -112,3 +94,45 @@ class PermissionPredicate extends Predicate {
   @override
   Future<bool> test(BuildContext context) => PermissionSupport.requestPermissions(context, permissions, gotoAppSettings: gotoAppSettings);
 }
+
+Widget _defaultFalseWidgetBuilder(BuildContext context) => Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text("拒绝响应 请刷新重试", style: TextStyle(fontSize: 46.sp)),
+        SizedBox(width: double.infinity, height: 10),
+        ElevatedButton(
+          child: Text("刷新"),
+          style: ButtonStyle(
+            textStyle: MaterialStateProperty.all(TextStyle(fontSize: 46.sp)),
+            shape: MaterialStateProperty.all(StadiumBorder()),
+            padding: MaterialStateProperty.all(EdgeInsets.fromLTRB(25, 0, 25, 0)),
+          ),
+          onPressed: () {
+            RebuildNotification().dispatch(context);
+          },
+        )
+      ],
+    );
+
+Widget _defaultWaitingWidgetBuilder(BuildContext context) => Center(child: CircularProgressIndicator());
+
+Widget _defaultFailedWidgetBuilder(BuildContext context, Object error, Widget? child) => Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text("加载失败 请刷新重试", style: TextStyle(fontSize: 46.sp)),
+        SizedBox(width: double.infinity, height: 10),
+        ElevatedButton(
+          child: Text("刷新"),
+          style: ButtonStyle(
+            textStyle: MaterialStateProperty.all(TextStyle(fontSize: 46.sp)),
+            shape: MaterialStateProperty.all(StadiumBorder()),
+            padding: MaterialStateProperty.all(EdgeInsets.fromLTRB(25, 0, 25, 0)),
+          ),
+          onPressed: () {
+            RebuildNotification().dispatch(context);
+          },
+        )
+      ],
+    );
